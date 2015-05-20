4658e15477de651a2238f54358b862320dbadd55
Luc Mazon <luc.mazon@gmail.com>
2015-05-20 07:25:54 +0200

    before argparse

--- gui.py
+++ gui.py
@@ -12,6 +12,7 @@ print data
 
 layout = data["layout"]
 grid = layout["grid"]
+banks = data["banks"]
 
 root = Tk()
 
@@ -20,48 +21,71 @@ pressed_color = 'red'
 
 current_bank = 0
 
+def unpack(i):
+    bank_grid = banks[current_bank]["grid"][i]
+    label = bank_grid["label"]
+    send = None
+    bank = None
+    try:
+        send = bank_grid["send"]
+    except KeyError, err:
+        pass
+    try:
+        bank = bank_grid["bank"]
+    except KeyError, err:
+        pass
+    row = grid[i][0]
+    column = grid[i][1]
+    return banks, bank_grid, label, send, bank, row, column
+    
 def callback(path, args):
     value = args[0]
     print "message received '%s' with value '%s'" % (path, value)
     for i, message_mapping in enumerate(layout["osc"]):
         if message_mapping[1] == value:
             print "message matches item %d" % i
-            draw_items(current_bank, i)
-
-def draw_items(bank_id, item_id=-1):
+            draw_items(i)
+            banks, bank_grid, label, send, bank, row, column = unpack(i)
+            update_bank(bank)
+            if send:
+                print "sending osc message '%s %s'" % (send[0], send[1])
+                liblo.send(target, send[0], send[1])
+                       
+def draw_items(item_id=-1):
     for i, item in enumerate(grid):
         current_color = default_color
 
-        banks = data["banks"]
-        bank_grid = banks[bank_id]["grid"][i]
-        label = bank_grid["label"]
-        row = grid[i][0]
-        column = grid[i][1]
+        banks, bank_grid, label, send, bank, row, column = unpack(i)
 
-        bank = 0
         if i == item_id:
-            try:
-                bank = bank_grid["bank"]
-            except KeyError, err:
-                pass
             current_color = pressed_color
             
         Label(text=label, bg=current_color, fg='white', width=15).grid(row=row,column=column)
 
-        global current_bank
-        current_bank = current_bank + bank
-        if current_bank >= len(data["banks"]):
+def update_bank(bank_value):
+    global current_bank
+    print bank_value
+    if bank_value == "next":
+        if current_bank == len(banks) - 1:
             current_bank = 0
+        else:
+            current_bank += 1
+    elif arg == "previous":
+        if current_bank == 0:
+            current_bank = len(banks) - 1
+        else:
+            current_bank -= 1
+
 
 def loop():
     result = server.recv(0)
     if not result:
-        draw_items(current_bank)
+        draw_items()
     root.after(50, loop)
 
 try:
     server = liblo.Server(8888)
-    target = liblo.Server(8000)
+    target = liblo.Address(8000)
 except liblo.ServerError, err:
     print err
     sys.exit()
